e784096caf2e159e3bec972f140737253ac8019d
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2022-12-29 11:59:59 -0600

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -6,73 +6,39 @@
 //
 
 import SwiftUI
+import Foundation
+
+
 
 struct ContentView: View {
-    
-    // Log Vars
-    
-    
-    @State private var AppleWatchWorn: Bool = false
-    
-    @State private var CaloriesBurnt: String = ""
-    
-    @State private var Duration: String = ""
-    
-    @State private var RingClosed: Bool = false
-    
-    @State private var AllRingsClosed: Bool = false
-    
-    @State private var WorkedHard: Bool = false
-    
-    
     var body: some View {
         
         
-        VStack {
-            
-            
-            Text("Log Your Workout")
-                .font(.title)
-                .bold()
-                .padding()
-                .padding(.bottom, 40)
-            
-            
+        NavigationView {
             
             VStack {
                 
-                // Form for workout
-                
-                
-                
-                Toggle("Apple Watch Worn", isOn: $AppleWatchWorn)
-                    .padding(.all)
-                
-                
-                TextField("Calories Burnt", text: $CaloriesBurnt)
-                    .padding(.all)
-                    .keyboardType(.decimalPad)
-                
-                TextField("Duration", text: $Duration)
-                    .padding(.all)
-//                    .keyboardType)(.)
-                
-                Toggle("One Ring Closed", isOn: $RingClosed)
-                    .padding(.all)
-                
-                Toggle("All Rings Closed", isOn: $AllRingsClosed)
-                    .padding(.all)
-                
-                Toggle("Worked Hard", isOn: $WorkedHard)
-                    .padding(.all)
-                
+                List {
+                    
+                    NavigationLink {
+                        LogWorkout()
+                    } label: {
+                        Text("Log Your Workout")
+                            .font(.title3)
+                    }
+                    
+                    NavigationLink {
+                        LogHistory()
+                    } label: {
+                        Text("Log History")
+                            .font(.title3)
+                    }
+                }.navigationTitle("Workout Logging")
             }
-            
-            Spacer()
         }
+        
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
